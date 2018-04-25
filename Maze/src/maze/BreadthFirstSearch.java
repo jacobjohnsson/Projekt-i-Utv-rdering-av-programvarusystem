b83ad2cf8f4ff79eb430cd587ff4752d6512e805
wetterbrandt <38552630+wetterbrandt@users.noreply.github.com>
2018-04-25 11:29:56 +0200

    Add files via upload

--- Maze/src/maze/BreadthFirstSearch.java
+++ Maze/src/maze/BreadthFirstSearch.java
@@ -1,9 +1,21 @@
 package maze;
 
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.TreeMap;
+
 public class BreadthFirstSearch extends MazeRunner {
-	
+	private Set<TreeCell> closedSet;
+	private Set<TreeCell> openSet;
+	private List<TreeCell> graph;
+
 	public BreadthFirstSearch(){
 		super();
+		closedSet = new HashSet<TreeCell>();
+		openSet = new HashSet<TreeCell>();
+		graph = new LinkedList<TreeCell>();
 	}
 
 	@Override
